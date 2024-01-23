a566e11a42ec88e2fb90757daa040049eaf49890
Dhaval Sojitra <143777610+dhaval-sojitra@users.noreply.github.com>
2024-01-23 11:56:42 +0530

    Add files via upload

---
+++ def6.py
@@ -0,0 +1,14 @@
+# binary to decimal
+num1 = "0b1001"
+dnum1 = int(num1,2)
+print("Binary to decimal : ",dnum1)
+
+#Octal to decimal
+num2  ="0o214"
+onum2 = int(num2,8)
+print("Octal to Decimal : ",onum2)
+
+#Hexadecimal to decimal
+num3 = "2e"
+hnum3 = int(num3,16)
+print("Hexadecimal to Decimal : ",hnum3)
