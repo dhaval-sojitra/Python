4ed4ff2c7d4d33847516d9da1807bf0e67e17f6a
Dhaval Sojitra <143777610+dhaval-sojitra@users.noreply.github.com>
2024-01-29 09:05:00 +0530

    Update script_2.py

--- practise set 1/script_2.py
+++ practise set 1/script_2.py
@@ -1,7 +1,7 @@
 # Write a program that prints your address with name, all in new lines
 
 
-my_name = "Rahul kanjariya"
+my_name = "Dhaval Sojitra"
 my_address = "Ram Mandir Street,\nJunanagna,\nJamnagar,\nGujarat"
 
 # Print name and address on separate lines
