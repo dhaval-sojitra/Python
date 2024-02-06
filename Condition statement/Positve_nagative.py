f244c32427991c7a436c4e1b0de0adf1ad949c52
Dhaval Sojitra <143777610+dhaval-sojitra@users.noreply.github.com>
2024-02-06 10:53:14 +0530

    Add files via upload

---
+++ Condition statement/Positve_nagative.py
@@ -0,0 +1,8 @@
+no = int(input('Enter Any Number :'))
+
+if no>0:
+    print("Given Numer %i is Positive"%no)
+elif no<0:
+    print("Given Numer %i is Nagative"%no)
+else:
+    print("Given Numer %i is Zero"%no)
