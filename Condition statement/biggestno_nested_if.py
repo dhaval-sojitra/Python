af8830a4ef0b8195a44ae0ddc4e5d8f859bc8791
Dhaval Sojitra <143777610+dhaval-sojitra@users.noreply.github.com>
2024-02-06 10:47:46 +0530

    Add files via upload

---
+++ Condition statement/biggestno_nested_if.py
@@ -0,0 +1,22 @@
+a=10
+b=25
+c=45
+d=89
+
+if a>b:
+    if a>c:
+        if a>d:
+            print('A is Biggest Number')
+if b>a:
+    if b>c:
+        if b>d:
+            print('B is Biggest Number')
+if c>a:
+    if c>b:
+        if c>d:
+            print('C is Biggest Number')
+
+if d>a:
+    if d>b:
+        if d>c:
+            print('D is Biggest Number')
