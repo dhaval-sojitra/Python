eb0b6a8e89245b1a46d1b96335ade6889a79a194
Dhaval Sojitra <dhavalsojitra875@gmail.com>
2024-03-04 23:33:24 +0530

    Create pro_14.py

---
+++ Practise set 4/pro_14.py
@@ -0,0 +1,10 @@
+print("Enter Numbers and get sum of given numbers,for stop insering numbers please enter 0.")
+a=1
+num = None
+sum = 0
+while num!=0:
+    num = int(input("Enter Number: "))
+    sum += num
+    a+=1
+print("Sum of your entered numbers is :",sum)
+   
