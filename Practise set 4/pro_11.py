372f0a09ca9ed4fae1cd3b6ad3aeb7f28557e3eb
Dhaval Sojitra <dhavalsojitra875@gmail.com>
2024-03-04 23:33:12 +0530

    Create pro_11.py

---
+++ Practise set 4/pro_11.py
@@ -0,0 +1,21 @@
+# 11) Display the following output using loop:
+# i. 1 to 10
+print("Print 1 to 10")
+for i in range(1, 11):
+    print(i)
+
+
+# ii. 10 to 1
+print( "\n\nPrint 10 to 1" )
+for i in range(10,0,-1):
+    print(i)
+
+# iii. 1 3 5 7 9\
+print("\n\nPrint 1 3 5 7 9")
+for i in range(1,11,2):
+    print(i)
+
+# iv. 2 4 6 8 10
+print("\n\nPrint 2 4 6 8 10")
+for i in range(2,11,2):
+    print(i)
