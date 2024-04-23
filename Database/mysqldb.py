8da583fb50d40318b935c3c3861d30799efff85d
unknown <dhavalsojitra875@gmail.com>
2024-04-23 11:39:12 +0530

    fdd

--- Database/mysqldb.py
+++ Database/mysqldb.py
@@ -15,15 +15,14 @@ mcursor = mydb.cursor()
 # mcursor.execute('create table marutable(id int(3) primary key auto_increment, name varchar(255))')
 
 # mcursor.execute("insert into marutable (id,name) values (2,'Dhaval Sojitra')")
-# query = "insert into marutable (id,name) values (%s,%s)"
-# val = [(3,'Jimit'),(4,'Keval')]
+query = "insert into marutable (id,name) values (%s,%s)"
+val = [(5,'Jimit'),(6,'Keval')]
+mcursor.executemany(query,val)
 
-# mcursor.executemany(query,val)
-
-mcursor.execute("select * from marutable")
-data = mcursor.fetchall()
-for i in data:
-    print(i)
+# mcursor.execute("select * from marutable")
+# data = mcursor.fetchall()
+# for i in data:
+#     print(i)
 
 # mcursor.execute("update marutable set name='keval' where id=4")
 # mcursor.execute("delete from marutable where id = 2 ")
